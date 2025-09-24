1df6c3306d9bd2fdf46ce22f4a26ba09dde9d074
gautam247gk <gautam247gk@gmail.com>
2025-09-24 22:11:43 +0100

    Add weather app functionality with geolocation and API integration

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   function error() {
     console.log("Unable to retrieve your location");
   }
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=895284fb2d2c50a520ea537456963d9c`;
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
@@ -62,7 +62,7 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
@@ -83,7 +83,7 @@ function App() {
             </div>
             <div className="wind">
               {data.wind ? (
-                <p className="bold">{data.wind.speed.toFixed()} KMPH</p>
+                <p className="bold">{data.wind.speed.toFixed()} m/s</p>
               ) : null}
               <p>Wind Speed</p>
             </div>
